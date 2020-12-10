f0df8991dd329051f972b5b9b004414f5eff9e2f
Andres O. Vela <andresovela@gmail.com>
2020-12-10 16:02:12 +0100

    Initial commit

---
+++ testsuite/tests/test.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use my_app as _; // memory layout + panic handler
+
+// See https://crates.io/crates/defmt-test/0.1.0 for more documentation (e.g. about the 'state'
+// feature)
+#[defmt_test::tests]
+mod tests {
+    #[test]
+    fn assert_true() {
+        defmt::assert!(true)
+    }
+
+    #[test]
+    fn assert_eq() {
+        defmt::assert_eq!(24, 42, "TODO: write actual tests")
+    }
+}
