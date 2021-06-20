1de01e330cc4d3280399e18e7dcd1c8887a96b44
Andres O. Vela <andresovela@gmail.com>
2021-06-20 15:24:14 +0200

    Implemented an example that loops changing colors and orientation, drawing some text too to demonstrate use with the `embedded-graphics` crate

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
 #![no_main]
 #![no_std]
 
-use display_interface_parallel_gpio::{
-    PGPIO8BitInterface, Generic8BitBus
-};
+use display_interface_parallel_gpio::{Generic8BitBus, PGPIO8BitInterface};
 
 use embedded_hal as _;
 use embedded_hal::blocking::delay::DelayMs;
 use hal::{gpio::Level, Delay};
-use ili9486::{Command, Orientation, ILI9486};
+use ili9486::{Command, Orientation, PixelFormat, ILI9486};
 use my_app as _;
 use nrf52832_hal as hal;
+
+use embedded_graphics::{
+    mono_font::{ascii::FONT_10X20, MonoTextStyle},
+    pixelcolor::Rgb565,
+    prelude::*,
+    text::Text,
+};
 
 #[cortex_m_rt::entry]
 fn main() -> ! {
@@ -29,16 +34,17 @@ fn main() -> ! {
     let ili9486_dcx = port0.p0_28.into_push_pull_output(Level::High);
     let _ili9486_rdx = port0.p0_03.into_push_pull_output(Level::High);
 
-    let ili9486_dbus = Generic8BitBus::new(
-        (port0.p0_19.into_push_pull_output(Level::Low).degrade(),
+    let ili9486_dbus = Generic8BitBus::new((
+        port0.p0_19.into_push_pull_output(Level::Low).degrade(),
         port0.p0_20.into_push_pull_output(Level::Low).degrade(),
         port0.p0_13.into_push_pull_output(Level::Low).degrade(),
         port0.p0_14.into_push_pull_output(Level::Low).degrade(),
         port0.p0_15.into_push_pull_output(Level::Low).degrade(),
         port0.p0_16.into_push_pull_output(Level::Low).degrade(),
         port0.p0_17.into_push_pull_output(Level::Low).degrade(),
-        port0.p0_18.into_push_pull_output(Level::Low).degrade())
-    ).unwrap();
+        port0.p0_18.into_push_pull_output(Level::Low).degrade(),
+    ))
+    .unwrap();
 
     let ili9486_interface = PGPIO8BitInterface::new(ili9486_dbus, ili9486_dcx, ili9486_wrx);
     let mut display = ILI9486::new(ili9486_interface, ili9486_csx, ili9486_rsx).unwrap();
@@ -60,10 +66,12 @@ fn main() -> ! {
     display.enable().unwrap();
 
     defmt::info!("Setting orientation");
-    display.set_orientation(Orientation::PortraitUpsideDown).unwrap();
+    display
+        .set_orientation(Orientation::PortraitUpsideDown)
+        .unwrap();
 
     defmt::info!("Setting pixel format");
-    display.set_pixel_format().unwrap();
+    display.set_pixel_format(PixelFormat::Rgb565).unwrap();
 
     defmt::info!("Sending sleep out");
     display.send_command(Command::SleepOut).unwrap();
@@ -74,17 +82,32 @@ fn main() -> ! {
     defmt::info!("Sending display on");
     display.send_command(Command::DisplayOn).unwrap();
 
+    // Create a new character style
+    let style = MonoTextStyle::new(&FONT_10X20, Rgb565::WHITE);
+
+    let mut colors = [0xF800u16, 0x07E0, 0x001F].iter().cycle();
+    let mut orientations = [
+        Orientation::Portrait,
+        Orientation::Landscape,
+        Orientation::PortraitUpsideDown,
+        Orientation::LandscapeUpsideDown,
+    ]
+    .iter()
+    .cycle();
+
     loop {
-        defmt::info!("Setting fill 1");
-        display.fill(0xF800).unwrap();
-        delay.delay_ms(1000u32);
+        display
+            .set_orientation(*orientations.next().unwrap())
+            .unwrap();
+        display.fill(*colors.next().unwrap()).unwrap();
 
-        defmt::info!("Setting fill 2");
-        display.fill(0x07E0).unwrap();
-        delay.delay_ms(1000u32);
-
-        defmt::info!("Setting fill 3");
-        display.fill(0x001F).unwrap();
+        // Create a text at position (20, 30) and draw it using the previously defined style
+        let text = Text::new(
+            "Hello Rust!",
+            Point::new(20, i32::from(display.height() / 2)),
+            style,
+        );
+        text.draw(&mut display).unwrap();
         delay.delay_ms(1000u32);
     }
 }
